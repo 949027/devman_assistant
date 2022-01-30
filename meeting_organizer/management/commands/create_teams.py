0532896eaca0eae7cdfe46a77d5bb9de1cc5f011
949027 <82878668+949027@users.noreply.github.com>
2022-01-30 20:37:39 +0700

    Fixed creating meetings with multiple product-managers

--- meeting_organizer/management/commands/create_teams.py
+++ meeting_organizer/management/commands/create_teams.py
@@ -8,15 +8,15 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         d = date(1900, 1, 1)
-        product_manager = ProductManager.objects.all().first()
-        meeting_start = datetime.combine(d, product_manager.worktime_from)
-        meeting_end = meeting_start + timedelta(minutes=30)
-
-        while meeting_end < datetime.combine(d, product_manager.worktime_to):
-            print(meeting_end)
-            meeting_start += timedelta(minutes=30)
+        for product_manager in ProductManager.objects.all():
+            meeting_start = datetime.combine(d, product_manager.worktime_from)
             meeting_end = meeting_start + timedelta(minutes=30)
-            Meeting.objects.create(
-                time=meeting_start,
-                product_manager=ProductManager.objects.all().first(),
-            )
+
+            while meeting_end <= datetime.combine(d, product_manager.worktime_to):
+                print(meeting_start)
+                meeting_start += timedelta(minutes=30)
+                meeting_end = meeting_start + timedelta(minutes=30)
+                Meeting.objects.create(
+                    time=meeting_start,
+                    product_manager=ProductManager.objects.all().first(),
+                )
