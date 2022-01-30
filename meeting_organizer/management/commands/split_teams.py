1377741b9357e732491a5dd0b5c5b4f0eef55b57
949027 <82878668+949027@users.noreply.github.com>
2022-01-30 20:43:09 +0700

    Added sending messages to free students

--- meeting_organizer/management/commands/split_teams.py
+++ meeting_organizer/management/commands/split_teams.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Count
 from datetime import timedelta, datetime, date
+import telegram
 
 from meeting_organizer.models import Meeting, Student
 
@@ -20,9 +21,13 @@ class Command(BaseCommand):
                 student.time_interval = worktime_to-worktime_from
                 student.save()
             students_queue = list(Student.objects.filter(level=level).order_by('time_interval'))
-
             for meeting in Meeting.objects.filter(team_members=None):
                 candidates = []
+
+                Meeting.objects.get(id=meeting.id)
+                meeting.level = level
+                meeting.save()
+
                 for student in students_queue:
                     if student.worktime_from <= meeting.time and student.worktime_to > meeting.time:
                         candidates.append(student)
@@ -33,5 +38,26 @@ class Command(BaseCommand):
                         meeting.team_members.through.objects.create(meeting_id=meeting.id, student_id=candidate.id)
                         students_queue.remove(candidate)
 
+                if candidates_amount == 2:
+                    for candidate in candidates[:2]:
+                        meeting.team_members.through.objects.create(meeting_id=meeting.id, student_id=candidate.id)
+                        students_queue.remove(candidate)
+
+                if candidates_amount < 2:
+                    Meeting.objects.get(id=meeting.id).delete()
+
             for student in students_queue:
-                free_students.append(student) # студенты которым не хватило места
+                free_students.append(student)
+
+        incomplete_teams = Meeting.objects.annotate(num_members=Count('team_members')).filter(num_members=2)
+        available_time = []
+        for team in incomplete_teams:
+            available_time.append(str(team.time))
+
+        #рассылка сообщений свободным ученикам
+        for student in free_students:
+            text = 'Привет! К сожалению, в удобное для тебя время созвонов нет.\n' \
+                   'Сможешь выбрать другой промежуток времени? Для этого нажми ' \
+                   f'кнопку "Другое время".\n Вот какое время доступно:\n {available_time}'
+            bot = telegram.Bot(token='5003258723:AAFUwJ_Jda918H9OU8Q9DWI1RRtAiw168yw')
+            bot.sendMessage(chat_id=student.telegram_chat_id, text=text)
